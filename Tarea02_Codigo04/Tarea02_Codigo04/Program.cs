b18319eb8c00528b9ec39a54b008c866ec328db0
Yessenia <106893050+Yessi0908@users.noreply.github.com>
2024-02-23 23:23:57 -0600

    Add files via upload

---
+++ Tarea02_Codigo04/Tarea02_Codigo04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Leerteclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de dos numeros");
+            Console.Write("Ingresa numero 1: ");
+            //los datos se leen como cadena
+            string dato = Console.ReadLine();
+            //Convertir numero con int.parse
+            int n1 = int.Parse(dato);
+
+            Console.Write("Ingresa numero 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("El resultado es: {0}", suma);
+
+            
+        }
+    }
+}
